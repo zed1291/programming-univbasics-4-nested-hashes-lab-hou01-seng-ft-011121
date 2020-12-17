ba1b3b7d68161b8848d47592a477c20d274123df
zed <zed@kolk.me>
2020-12-17 23:04:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -69,26 +69,25 @@ def adding_matz
 # }
 # return the entire updated hash
 
-	programmer_hash =
- 		{
-      :grace_hopper => {
-        :known_for => "COBOL",
-        :languages => ["COBOL", "FORTRAN"]
-      },
-      :alan_kay => {
-        :known_for => "Object Orientation",
-        :languages => ["Smalltalk", "LISP"]
-      },
-      :dennis_ritchie => {
-        :known_for => "Unix",
-        :languages => ["C"]
-				}
-      }
-    }
-		programmer_hash[:yukihiro_matsumoto] = {
-    :known_for => "Ruby",
-    :languages => ["LISP", "C"]
-    }
+programmer_hash =
+	{
+		:grace_hopper => {
+			:known_for => "COBOL",
+			:languages => ["COBOL", "FORTRAN"]
+		},
+		:alan_kay => {
+			:known_for => "Object Orientation",
+			:languages => ["Smalltalk", "LISP"]
+		},
+		:dennis_ritchie => {
+			:known_for => "Unix",
+			:languages => ["C"]
+			}
+		}
+programmer_hash[:yukihiro_matsumoto] = {
+	:known_for => "Ruby",
+	:languages => ["LISP", "C"]
+	}
 
 end
 
